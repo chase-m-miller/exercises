16e4cc1ea85a82abd157eceeed06a2cfa6dcfec4
Chase Miller <chasemmiller2@gmail.com>
2024-02-01 17:29:14 -0600

    Refactored to use existing function strlen()

--- char-counter.c
+++ char-counter.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 #define STRING_MAX 256
 
 int main(void)
 {
-	int count;
+	int i;
 	char c;
 	char user_string[STRING_MAX];
 
 	printf("What is the input string? ");
-	for (count = 0; (c = getchar()) != '\n'; ++count)
+	for (i = 0; (c = getchar()) != '\n'; ++i)
 	{
-		if (count >= STRING_MAX)
+		if (i >= STRING_MAX)
 		{
 			printf("Your string exceeds the maximum character limit of %i.\n", STRING_MAX - 1);
 			return 0;
 		}
-		user_string[count] = c;
+		user_string[i] = c;
 	}
-	user_string[count] = '\0';
+	user_string[i] = '\0';
 
-	printf("The string %s is %i characters long.\n", user_string, count);
+	printf("The string %s is %i characters long.\n", user_string, (int) strlen(user_string));
 	return 0;
 }
