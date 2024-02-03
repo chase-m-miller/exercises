66a564d893c2707bfc99cd2750435ddd5ba6a025
Chase Miller <chasemmiller2@gmail.com>
2024-02-02 21:42:14 -0600

    Add get_word function

--- madlib.c
+++ madlib.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+void get_word(char prompt[], char word[], int wordlen)
+{
+	printf("%s", prompt);
+	fgets(word, wordlen, stdin);
+	word[strcspn(word, "\n")] = 0;
+
+	return;
+}
+
 int main(void)
 {
 	const int STR_MAX = 50;
@@ -10,21 +19,10 @@ int main(void)
 	char adjective[STR_MAX];
 	char adverb[STR_MAX];
 
-	printf("Enter a noun: ");
-	fgets(noun, STR_MAX, stdin);
-	noun[strcspn(noun, "\n")] = 0;
-
-	printf("Enter a verb: ");
-	fgets(verb, STR_MAX, stdin);
-	verb[strcspn(verb, "\n")] = 0;
-
-	printf("Enter an adjective: ");
-	fgets(adjective, STR_MAX, stdin);
-	adjective[strcspn(adjective, "\n")] = 0;
-
-	printf("Enter an adverb: ");
-	fgets(adverb, STR_MAX, stdin);
-	adverb[strcspn(adverb, "\n")] = 0;
+	get_word("Enter a noun: ", noun, STR_MAX);
+	get_word("Enter a verb: ", verb, STR_MAX);
+	get_word("Enter an adjective: ", adjective, STR_MAX);
+	get_word("Enter an adverb: ", adverb, STR_MAX);
 
 	printf("Do you always %s your %s %s so %s? That's... interesting...\n", verb, adjective, noun, adverb);
 
