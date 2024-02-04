2cca7e3e4ec6ce3edc9b81ea2220247694ab16ff
Chase Miller <chasemmiller2@gmail.com>
2024-02-03 18:17:25 -0600

    Add isnum function

--- simple-math.c
+++ simple-math.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int isnum(char num_str[])
+{
+	for (int i = 0; i < strlen(num_str) - 1; ++i)
+	{
+		if (num_str[i] < 48 || num_str[i] > 57)
+		{
+			return(1);
+		}
+	}
+
+	return 0;
+}
 
 int main(void)
 {
@@ -10,9 +24,19 @@ int main(void)
 
 	printf("What is the first number? ");
 	fgets(a_str, MAX_DIGITS, stdin);
+	if (isnum(a_str) != 0)
+	{
+		printf("Please enter numbers only.\n");
+		return 1;
+	}
 
 	printf("What is the second number? ");
 	fgets(b_str, MAX_DIGITS, stdin);
+	if (isnum(a_str) != 0)
+	{
+		printf("Please enter numbers only.\n");
+		return 1;
+	}
 
 	int a = atoi(a_str);
 	int b = atoi(b_str);
