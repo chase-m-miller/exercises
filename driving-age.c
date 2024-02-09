987f8938542c649847f55a5cb4d32a58ee3f1730
Chase Miller <chasemmiller2@gmail.com>
2024-02-09 15:38:37 -0600

    Add driving age test

---
+++ driving-age.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	const int STR_MAX = 32;
+	char input[STR_MAX];
+	int age;
+
+	printf("What is your age? ");
+	fgets(input, STR_MAX, stdin);
+	age = atoi(input);
+
+	char *answer = (age > 15) ? "You are old enough to drive.\n" : "You are not old enough to drive.\n";
+	printf("%s", answer);
+
+	return 0;
+}
