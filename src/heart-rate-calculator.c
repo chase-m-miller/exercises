ac0a11811644953c77e84da292a557bb92ddf481
Chase Miller <chasemmiller2@gmail.com>
2024-02-15 20:13:51 -0600

    Add heart-rate-calculator program

---
+++ src/heart-rate-calculator.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXLINE 5
+
+int calculate_heart_rate(int age, int resting_hr, double intensity) {
+	return (((220 - age) - resting_hr) * intensity) + resting_hr;
+}
+
+int main(void) {
+	char input_buffer[MAXLINE];
+	int age;
+	int resting_hr;
+
+	printf("What is your age? ");
+	fgets(input_buffer, MAXLINE, stdin);
+	age = atoi(input_buffer);
+
+	printf("What is your resting heart rate? ");
+	fgets(input_buffer, MAXLINE, stdin);
+	resting_hr = atoi(input_buffer);
+
+	printf("Resting pulse: %i\tAge: %i\n", resting_hr, age);
+	for (double intensity = .55; intensity <= .95; intensity += .05) {
+		printf("%.0f\t%i bpm\n", intensity * 100.0, calculate_heart_rate(age, resting_hr, intensity));
+	}
+
+	return 0;
+}
