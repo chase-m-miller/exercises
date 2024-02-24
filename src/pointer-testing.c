e280cc794a83525b514faba0c636de6b00ff0c9f
Chase Miller <chasemmiller2@gmail.com>
2024-02-24 16:10:23 -0600

    Reformat comments to read easier

--- src/pointer-testing.c
+++ src/pointer-testing.c
@@ -8,11 +8,11 @@ int main(void) {
 	int rows = 0;
 	char **strings = NULL;
 
-	//strings = malloc(rows * sizeof(*strings));
-	//strings[0] = malloc(sizeof(**strings) * MAXLINE);
-	//strings[1] = malloc(sizeof(**strings) * MAXLINE);
-	//fgets(strings[0], MAXLINE, stdin);
-	//fgets(strings[1], MAXLINE, stdin);
+	// strings = malloc(rows * sizeof(*strings));
+	// strings[0] = malloc(sizeof(**strings) * MAXLINE);
+	// strings[1] = malloc(sizeof(**strings) * MAXLINE);
+	// fgets(strings[0], MAXLINE, stdin);
+	// fgets(strings[1], MAXLINE, stdin);
 
 	do {
 		strings = realloc(strings, ++rows * sizeof(*strings));
@@ -24,8 +24,8 @@ int main(void) {
 		printf("%s", strings[i]);
 	}
 
-	//free(strings[0]);
-	//free(strings[1]);
+	// free(strings[0]);
+	// free(strings[1]);
 	
 	for (int i = 0; i < rows; ++i) {
 		free(strings[i]);
