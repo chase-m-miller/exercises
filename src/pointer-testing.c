1486c13284b9494e0915b72c0e371adcf08a191c
Chase Miller <chasemmiller2@gmail.com>
2024-02-23 21:22:00 -0600

    still screamin' :^)

--- src/pointer-testing.c
+++ src/pointer-testing.c
@@ -7,18 +7,22 @@
 int main(void) {
 	int num_of_strings = 1;
 	char input_buffer[MAXLINE]; 
-	char **strings;
+	char **strings = NULL;
 
-	fgets(input_buffer, MAXLINE, stdin);
-	input_buffer[strcspn(input_buffer, "\n") + 1] = '\0';
+	strings = malloc(MAXLINE * sizeof(char) + 2);
+	printf("HERE\n");
+	fgets(strings[0], MAXLINE, stdin);
+	*strings[strcspn(strings[0], "\n") + 1] = '\0';
 
-	printf("%s", input_buffer);
-
-	*strings = malloc(sizeof(*strings) * strlen(input_buffer));
-	printf("Made it here\n");
-	strcpy(strings[0], input_buffer);
+	printf("OVER HERE\n");
 
 	printf("%s", strings[0]);
+
+	printf("Made it here\n");
+	strcpy(*strings, input_buffer);
+	printf("And here\n");
+
+	printf("%s", *strings);
 
 	return 0;
 }
