9d775e8c12788e09786fe8cf7b85446802417293
Chase Miller <chasemmiller2@gmail.com>
2024-02-23 21:41:19 -0600

    WE'RE GOING PLACES

--- src/pointer-testing.c
+++ src/pointer-testing.c
@@ -5,13 +5,17 @@
 #define MAXLINE 32
 
 int main(void) {
+	int rows = 2;
 	char **strings = NULL;
 
-	strings = malloc(sizeof(*strings));
+	strings = malloc(rows * sizeof(*strings));
 	strings[0] = malloc(sizeof(**strings) * MAXLINE);
+	strings[1] = malloc(sizeof(**strings) * MAXLINE);
 	fgets(strings[0], MAXLINE, stdin);
+	fgets(strings[1], MAXLINE, stdin);
 
 	printf("%s", strings[0]);
+	printf("%s", strings[1]);
 
 	return 0;
 }
