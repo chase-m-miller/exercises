135bff0f811e89eba53564a7e53ebbd3f21a4090
Chase Miller <chasemmiller2@gmail.com>
2024-02-23 21:37:39 -0600

    I think I'm onto something...

--- src/pointer-testing.c
+++ src/pointer-testing.c
@@ -5,24 +5,13 @@
 #define MAXLINE 32
 
 int main(void) {
-	int num_of_strings = 1;
-	char input_buffer[MAXLINE]; 
 	char **strings = NULL;
 
-	strings = malloc(MAXLINE * sizeof(char) + 2);
-	printf("HERE\n");
+	strings = malloc(sizeof(*strings));
+	strings[0] = malloc(sizeof(**strings) * MAXLINE);
 	fgets(strings[0], MAXLINE, stdin);
-	*strings[strcspn(strings[0], "\n") + 1] = '\0';
-
-	printf("OVER HERE\n");
 
 	printf("%s", strings[0]);
-
-	printf("Made it here\n");
-	strcpy(*strings, input_buffer);
-	printf("And here\n");
-
-	printf("%s", *strings);
 
 	return 0;
 }
