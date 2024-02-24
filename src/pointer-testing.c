60869b284953611c4ee6edd1064af68adbe21bad
Chase Miller <chasemmiller2@gmail.com>
2024-02-24 16:07:34 -0600

    LOOKS LIKE MEATS BACK ON THE MENU BOYS

--- src/pointer-testing.c
+++ src/pointer-testing.c
@@ -5,20 +5,31 @@
 #define MAXLINE 32
 
 int main(void) {
-	int rows = 2;
+	int rows = 0;
 	char **strings = NULL;
 
-	strings = malloc(rows * sizeof(*strings));
-	strings[0] = malloc(sizeof(**strings) * MAXLINE);
-	strings[1] = malloc(sizeof(**strings) * MAXLINE);
-	fgets(strings[0], MAXLINE, stdin);
-	fgets(strings[1], MAXLINE, stdin);
+	//strings = malloc(rows * sizeof(*strings));
+	//strings[0] = malloc(sizeof(**strings) * MAXLINE);
+	//strings[1] = malloc(sizeof(**strings) * MAXLINE);
+	//fgets(strings[0], MAXLINE, stdin);
+	//fgets(strings[1], MAXLINE, stdin);
 
-	printf("%s", strings[0]);
-	printf("%s", strings[1]);
+	do {
+		strings = realloc(strings, ++rows * sizeof(*strings));
+		strings[rows - 1] = malloc(sizeof(**strings) * MAXLINE);
+		fgets(strings[rows - 1], MAXLINE, stdin);
+	} while (strings[rows - 1][0] != '\n');
 
-	free(strings[0]);
-	free(strings[1]);
+	for (int i = 0; i < rows; ++i) {
+		printf("%s", strings[i]);
+	}
+
+	//free(strings[0]);
+	//free(strings[1]);
+	
+	for (int i = 0; i < rows; ++i) {
+		free(strings[i]);
+	}
 	free(strings);
 
 	return 0;
