e778168d3273df69ccff05e31b305a1c8add504c
Chase Miller <chasemmiller2@gmail.com>
2024-02-25 16:59:30 -0600

    Clarify variable name

--- src/raffle.c
+++ src/raffle.c
@@ -7,30 +7,30 @@
 
 int main(void) {
 	int names = 0;
-	char **raffle_entries = NULL;
+	char **raffle = NULL;
 
 	do {
-		raffle_entries = realloc(raffle_entries, ++names * sizeof(*raffle_entries));
-		raffle_entries[names - 1] = malloc(sizeof(**raffle_entries) * MAXLINE);
+		raffle = realloc(raffle, ++names * sizeof(*raffle));
+		raffle[names - 1] = malloc(sizeof(**raffle) * MAXLINE);
 		printf("Enter a name: ");
-		fgets(raffle_entries[names - 1], MAXLINE, stdin);
-	} while (raffle_entries[names - 1][0] != '\n');
+		fgets(raffle[names - 1], MAXLINE, stdin);
+	} while (raffle[names - 1][0] != '\n');
 
 	for (int i = 0; i < names; ++i) {
-		raffle_entries[i][strcspn(raffle_entries[i], "\n")] = '\0';
+		raffle[i][strcspn(raffle[i], "\n")] = '\0';
 	}
 
 	// free the final newline and correct the 'names' value
-	free(raffle_entries[--names]);
+	free(raffle[--names]);
 
 	srand(time(NULL));
 	int winner = rand() % names;
 
-	printf("The winner is %s!\n", raffle_entries[winner]);
+	printf("The winner is %s!\n", raffle[winner]);
 
-	free(raffle_entries);
+	free(raffle);
 	for (int i = 0; i < names; ++i) {
-		free(raffle_entries[i]);
+		free(raffle[i]);
 	}
 
 	return 0;
