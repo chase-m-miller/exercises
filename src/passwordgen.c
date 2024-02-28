2682c252ebd74aeb4e995477813231f48ddb310b
Chase Miller <chasemmiller2@gmail.com>
2024-02-28 15:50:31 -0600

    Add variables necessary for input

--- src/passwordgen.c
+++ src/passwordgen.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAXLINE 22
+
 int main(void) {
 	char alphabet[26];
 	char numbers[10];
 	char symbols[] = {'!', '@', '#', '$'};
+	char input_buffer[MAXLINE];
+	int pass_len;
+	int special_chars;
+	int nums;
 
 	for (int i = 0; i < 26; ++i) {
 		alphabet[i] = i + 'a';
