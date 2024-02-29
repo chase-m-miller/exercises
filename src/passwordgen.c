b32de1d66abfa0b90e09c2a3c769595f30774f2b
Chase Miller <chasemmiller2@gmail.com>
2024-02-29 15:31:47 -0600

    Complete passgen function

--- src/passwordgen.c
+++ src/passwordgen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define MAXLINE 22
 
@@ -40,11 +41,11 @@ void passgen(char *password, int pass_len, int special_chars, int nums) {
 	char alphabet[26];
 	char numbers[10];
 	char symbols[] = {'!', '@', '#', '$'};
+	int index;
 
 	for (int i = 0; i < 26; ++i) {
 		alphabet[i] = i + 'a';
 	}
-
 	for (int i = 0; i < 10; ++i) {
 		numbers[i] = i + '0';
 	}
@@ -52,4 +53,25 @@ void passgen(char *password, int pass_len, int special_chars, int nums) {
 	for (int i = 0; i < pass_len; ++i) {
 		password[i] = alphabet[rand() % 26];
 	}
+	password[pass_len] = '\0';
+
+	int nums_inserted = 0;
+	while (nums_inserted < nums) {
+		index = rand() % pass_len;
+		if (password[index] < '0' || password[index] > '9') {
+			password[index] = alphabet[rand() % 26];
+			++nums_inserted;
+		}
+	}
+
+	int symbols_inserted = 0;
+	while (symbols_inserted < special_chars) {
+		index = rand() % pass_len;
+		if (strchr(symbols, password[index]) == NULL && strchr(numbers, password[index]) == NULL) {
+			password[index] = symbols[rand() % 4];
+			++symbols_inserted;
+		}
+	}
+
+	return;
 }
