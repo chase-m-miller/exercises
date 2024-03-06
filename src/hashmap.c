04ec9f130b72af7ce566f11747f399617f0ebc5c
Chase Miller <chasemmiller2@gmail.com>
2024-03-05 20:33:25 -0600

    Add hashing function

--- src/hashmap.c
+++ src/hashmap.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+#define CAPACITY 50000 // Size of the hash table
 
+unsigned int hash_function(char* str) {
+	unsigned int hash_value = 0;
+
+	for (int i = 0; str[i]; ++i) {
+		hash_value += str[i];
+	}
+
+	return hash_value % CAPACITY;
 }
