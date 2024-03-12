fd0f46c4a2cad706f454b9470d5afa124c71960b
Chase Miller <chasemmiller2@gmail.com>
2024-03-12 14:48:36 -0500

    Add int array initializer function

--- src/filter-input.c
+++ src/filter-input.c
@@ -4,6 +4,13 @@
 
 #define MAXLINE 128
 
+void init_int_array(int array[], int size) {
+	for (int i = 0; i < size; ++i) {
+		array[i] = 0;
+	}
+	return;
+}
+
 int main(void) {
 	char input[MAXLINE];
 	int nums[MAXLINE];
