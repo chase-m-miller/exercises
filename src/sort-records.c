8811e8201d02e0475a028624989e2771f6326ec1
Chase Miller <chasemmiller2@gmail.com>
2024-03-15 19:56:42 -0500

    Add test sqlite3 implementation

--- src/sort-records.c
+++ src/sort-records.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sqlite3.h>
 
 #define ARR_MAX 256
 
@@ -12,5 +13,46 @@ struct Employee {
 
 int main(void) {
 	struct Employee employee_list[ARR_MAX];
+	sqlite3 *db;
+	sqlite3_stmt *stmt;
 
+	sqlite3_open("employees.db", &db);
+
+	if (db == NULL) {
+		printf("Failed to open DB\n");
+		return 1;
+	}
+
+	printf("Performing query...\n");
+	sqlite3_prepare_v2(db, "select * from people", -1, &stmt, NULL);
+
+	printf("Got results:\n");
+	while (sqlite3_step(stmt) != SQLITE_DONE) {
+		int i;
+		int num_cols = sqlite3_column_count(stmt);
+
+		for (i = 0; i < num_cols; ++i) {
+			switch (sqlite3_column_type(stmt, i)) {
+				case (SQLITE3_TEXT):
+					printf("%s, ", sqlite3_column_text(stmt, i));
+					break;
+				case (SQLITE_INTEGER):
+					printf("%d, ", sqlite3_column_int(stmt, i));
+					break;
+				case (SQLITE_FLOAT):
+					printf("%g, ", sqlite3_column_double(stmt, i));
+					break;
+				default:
+					break;
+			}
+		}
+		printf("\n");
+	}
+
+	sqlite3_finalize(stmt);
+
+	sqlite3_close(db);
+
+	getc(stdin);
+	return 0;
 }
