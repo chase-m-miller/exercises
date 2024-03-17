66c6576119bcd673cf07b2a7ac83093da4ce0885
Chase Miller <chasemmiller2@gmail.com>
2024-03-17 12:33:09 -0500

    The problem is definitely NOT in the sqlite3_open function

--- src/sort-records.c
+++ src/sort-records.c
@@ -16,7 +16,7 @@ int main(void) {
 	sqlite3_stmt *stmt;
 	int err;
 
-	sqlite3_open("file:~/Projects/exercises/src/employees.db", &db);
+	sqlite3_open("file:/home/chasemiller/Projects/exercises/src/employees.db", &db);
 
 	if (db == NULL) {
 		printf("Failed to open DB\n");
